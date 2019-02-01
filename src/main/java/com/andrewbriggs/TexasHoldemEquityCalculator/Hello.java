cd28fe50f2a58d1234060a7cb7422fa08ef08cf4
Andrew Briggs <andbrig14@gmail.com>
2019-01-31 18:03:37 -0600

    First commit

---
+++ src/main/java/com/andrewbriggs/TexasHoldemEquityCalculator/Hello.java
@@ -0,0 +1,11 @@
+package com.andrewbriggs.TexasHoldemEquityCalculator;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Hello {
+	
+	public void output() {
+		System.out.println("hello");
+	}
+}
